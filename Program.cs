d731b7425a5cb7e796bafe0ae089e2c69990045b
I7eTp <116294989+I7eTp@users.noreply.github.com>
2022-12-11 16:14:17 +0500

    63 65 67 69

--- Program.cs
+++ Program.cs
@@ -619,6 +619,63 @@ int sum =0;
      Console.WriteLine($"Сумма = {sum}");
 }
  */
+// ________________________Задача 63:___________________________
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+/* Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(PrintNumbers(1,N));
+
+string PrintNumbers(int start, int end)
+{
+    if(start == end) return start.ToString();
+    return(start + " " + PrintNumbers(start + 1, end));
+}
+ */
+//______________________________________ Задача 65:______________________________
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 5, 6, 7, 8"
+/* / Console.Write("введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine(PrintNumber(m,n));
+
+string PrintNumber(int start, int end)
+{
+    if (start==end) return start.ToString();
+    return(start + " " + PrintNumber(start + 1, end));
+}  */
+//____________________________________ Задача 67:_________________________________________
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+/* Console.Write("введите число N: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNumber(number));
+
+int SumNumber(int num)
+{
+    if (num==0) return 0;
+    return (num%10 + SumNumber(num/10));
+} */
+//____________________________________ Задача 69:__________________________________________
+// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+/* Console.Write("введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine(DegreeNumber(A,B));
+
+int DegreeNumber(int A, int B)
+{
+    if(B==0)return 1;
+    if (B == 1) return A;
+    return (A * DegreeNumber(A,B-1));
+} */
 
 
 
